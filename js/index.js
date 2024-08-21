5d781bdd3ce714a7254d8a3f519a5f5d1525c72e
paulHan <wtg.paulhan@gmail.com>
2024-08-21 17:07:03 +0900

    test commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
+const xhr = new XMLHttpRequest();
+const url = '/md/index.md';
 
-$.get('./md/index.md', function(data) {
-    $("pre").text(data);
-});
+xhr.open("GET", url);
+
+xhr.onreadystatechange = () => {
+    // In local files, status is 0 upon success in Mozilla Firefox
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      const status = xhr.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        const pre = document.querySelector('pre');
+        pre.innerText = xhr.responseText;
+      } else {
+        console.log('error');
+      }
+    }
+  };
+
+  xhr.send();
